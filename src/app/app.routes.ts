adb9828b11d6ed7b68ae7c70379f9bd6fe2ed18a
Kevin Mesiab <kmesiab@gmail.com>
2024-03-11 22:10:11 -0700

    Add OTP component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login-component/login.component';
-import { SignupComponent } from './components/signup-component/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import {OtpComponent} from "./components/otp/otp.component";
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'otp', component: OtpComponent}
 ];
